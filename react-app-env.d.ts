f63cf270156f99fbd9ccabdf3a025e10210c2564
jaywcjlove <398188662@qq.com>
2021-08-22 22:08:37 +0800

    chore: move src/react-app-env.d.ts -> react-app-env.d.ts

--- react-app-env.d.ts
+++ react-app-env.d.ts
@@ -4,3 +4,8 @@ declare module '*.module.less' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+declare module '*.md' {
+  const src: string;
+  export default src;
+}
--- website/react-app-env.d.ts
+++ website/react-app-env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="react-scripts" />
-
-declare module '*.module.less' {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
-
-declare module '*.md' {
-  const src: string;
-  export default src;
-}
