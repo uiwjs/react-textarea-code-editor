84069cf2c54dc86f0270c7d2f586446cb5f0462e
jaywcjlove <398188662@qq.com>
2022-02-28 09:05:36 +0800

    feat: output bundle file.

--- .kktrc.ts
+++ .kktrc.ts
@@ -7,42 +7,54 @@ import scopePluginOptions from '@kkt/scope-plugin-options';
 import pkg from './package.json';
 
 export default (conf: Configuration, env: 'development' | 'production', options: LoaderConfOptions) => {
-  conf = rawModules(conf, env, { ...options, test: /\.(txt|md)$/i });
-  conf = scopePluginOptions(conf, env, {
-    ...options,
-    allowedFiles: [path.resolve(process.cwd(), 'README.md'), path.resolve(process.cwd(), 'src')],
-  });
   conf = lessModules(conf, env, options);
-  // Get the project version.
-  conf.plugins!.push(
-    new webpack.DefinePlugin({
-      VERSION: JSON.stringify(pkg.version),
-    }),
-  );
-  if (env === 'production') {
-    conf.optimization = {
-      ...conf.optimization,
-      splitChunks: {
-        cacheGroups: {
-          reactvendor: {
-            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-            name: 'react-vendor',
-            chunks: 'all',
-          },
-          refractor: {
-            test: /[\\/]node_modules[\\/](refractor)[\\/]/,
-            name: 'refractor-vendor',
-            chunks: 'all',
-          },
-          codeexample: {
-            test: /[\\/]node_modules[\\/](code-example)[\\/]/,
-            name: 'code-example-vendor',
-            chunks: 'async',
-          },
-        },
+  if (options.bundle) {
+    conf.output!.library = '@uiw/react-textarea-code-editor';
+    conf.externals = {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
       },
     };
-    conf.output = { ...conf.output, publicPath: './' };
+  } else {
+    conf = rawModules(conf, env, { ...options, test: /\.(txt|md)$/i });
+    conf = scopePluginOptions(conf, env, {
+      ...options,
+      allowedFiles: [path.resolve(process.cwd(), 'README.md'), path.resolve(process.cwd(), 'src')],
+    });
+    // Get the project version.
+    conf.plugins!.push(
+      new webpack.DefinePlugin({
+        VERSION: JSON.stringify(pkg.version),
+      }),
+    );
+    if (env === 'production') {
+      conf.optimization = {
+        ...conf.optimization,
+        splitChunks: {
+          cacheGroups: {
+            reactvendor: {
+              test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+              name: 'react-vendor',
+              chunks: 'all',
+            },
+            refractor: {
+              test: /[\\/]node_modules[\\/](refractor)[\\/]/,
+              name: 'refractor-vendor',
+              chunks: 'all',
+            },
+            codeexample: {
+              test: /[\\/]node_modules[\\/](code-example)[\\/]/,
+              name: 'code-example-vendor',
+              chunks: 'async',
+            },
+          },
+        },
+      };
+      conf.output = { ...conf.output, publicPath: './' };
+    }
   }
   return conf;
 };
