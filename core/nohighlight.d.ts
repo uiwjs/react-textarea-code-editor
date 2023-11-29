022189cd57977569b260cfe7dfd144b7b68d81d2
jaywcjlove <398188662@qq.com>
2023-11-29 12:45:11 +0800

    type: fix nohighlight component type error.

---
+++ core/nohighlight.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+import { TextareaCodeEditorProps } from '@uiw/react-textarea-code-editor/esm/Editor';
+declare const _default: React.ForwardRefExoticComponent<
+  TextareaCodeEditorProps & React.RefAttributes<HTMLTextAreaElement>
+>;
+export default _default;
