55ee30fab274bcfaa744ebdd72cb0142c42efb3e
jaywcjlove <398188662@qq.com>
2023-11-30 10:14:28 +0800

    fix(type): fix export type issue. (#170)

--- core/src/index.nohighlight.tsx
+++ core/src/index.nohighlight.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Editor, { TextareaCodeEditorProps } from './Editor';
 
+export * from './Editor';
+
 export default React.forwardRef<HTMLTextAreaElement, TextareaCodeEditorProps>((props, ref) => {
   return <Editor {...props} ref={ref} />;
 });
--- core/src/index.tsx
+++ core/src/index.tsx
@@ -3,6 +3,8 @@ import { Pluggable } from 'unified';
 import Editor, { TextareaCodeEditorProps } from './Editor';
 import rehypePrism from 'rehype-prism-plus';
 
+export * from './Editor';
+
 export default React.forwardRef<HTMLTextAreaElement, TextareaCodeEditorProps>((props, ref) => {
   const { rehypePlugins = [[rehypePrism, { ignoreMissing: true }]] as Pluggable[], ...reset } = props;
   return <Editor {...reset} rehypePlugins={rehypePlugins} ref={ref} />;
